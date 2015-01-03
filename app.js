419eac48e8dc5b1382a75c486f51ee755ea75dca
Chanakya Devraj <chanakya.devraj@cuelogic.com>
2015-01-03 07:55:06 +0530

    till usernames

---
+++ app.js
@@ -0,0 +1,48 @@
+// Server side javascript for socket io
+
+var express = require('express'),
+ app = express(),
+ server = require('http').createServer(app),
+ io = require('socket.io').listen(server),
+ nicknames = []; //creating array to grap all the users and display them
+
+server.listen(3000); 
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname+'/index.html');
+})
+
+// getting messages server side 
+io.sockets.on('connection', function(socket){
+	
+	//username check on establishing connection
+	socket.on('new user', function(data, callback){
+		if (nicknames.indexOf(data) != -1){ // is matched
+			callback(false); // that is matched or can send customdata eg (isValid: false)
+		}else{
+			callback(true);
+			socket.nickname = data; //
+			nicknames.push(socket.nickname);
+			updateNicknames();
+
+		}
+	});
+
+	//send messages on establishing connection
+	socket.on('send message', function(data){ 
+		io.sockets.emit('new message', {msg: data, nick: socket.nickname}); //except us
+		// socket.broadcast.emit # send to every once but us
+	});
+
+
+	socket.on('disconnect', function(data){
+		if (!socket.nickname) return;
+		//splice(index,params(how many shall be remove from index))
+		nickname.splice(nicknames.indexOf(socket.nickname), 1);
+		updateNicknames();
+	});
+
+	function updateNicknames () {
+		io.sockets.emit('usernames', nicknames); // so that user can update some one new joined
+	};
+});
